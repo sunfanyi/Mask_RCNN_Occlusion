6f416efd168ce60c8eb0d183edb432da98bc9405
sunfanyi <sunfanyi_jim@outlook.com>
2023-03-06 16:24:10 +0000

    Save sample masks in json for easier debugging when calculating brdy_score, version 2.

--- occlusion/calc_bdry_score.py
+++ occlusion/calc_bdry_score.py
@@ -11,11 +11,17 @@ import sys
 import json
 import numpy as np
 
+ROOT_DIR = os.path.abspath("../")
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.utils import expand_mask
+
 sample_info = json.load(open('sample_mask_info.json', 'r'))
 
-images = [i['image'] for i in sample_info]
-images = np.array(images)
-mask_true = [i['mask_gt'] for i in sample_info]
-mask_true = np.array(mask_true)
+ids = [i['id'] for i in sample_info]
+file_names = [i['file_names'] for i in sample_info]
+mask_true = [i['mask_true'] for i in sample_info]
+mask_true = np.array(mask_true, dtype=np.bool)
 mask_pred = [i['mask_pred'] for i in sample_info]
-mask_pred = np.array(mask_pred)
+mask_pred = np.array(mask_pred, dtype=np.bool)
+
+
--- occlusion/save_sample_mask.py
+++ occlusion/save_sample_mask.py
@@ -17,12 +17,10 @@ import numpy as np
 
 import occlusion
 
-# Root directory of the project
 ROOT_DIR = os.path.abspath("../")
-
-# Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
 import mrcnn.model as modellib
+from mrcnn.utils import extract_bboxes, minimize_mask
 
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
@@ -32,6 +30,7 @@ MODEL_PATH = r"D:\Desktop\FYP - Robust Surgical Tool Detection and Occlusion Han
 config = occlusion.OcclusionConfig()
 dataset_DIR = '../../datasets/dataset_occluded'
 
+# sample_info = {'images': [], 'annotations': []}
 sample_info = []
 
 # ==================== Get gt mask ========================
@@ -41,28 +40,39 @@ dataset.prepare()
 
 # bottleFGL1_BGL1/n02823428_2278, carFGL1_BGL1/n02814533_3155
 # aeroplaneFGL1_BGL1/n02690373_3378 trivial case
-target_id_1 = "aeroplaneFGL1_BGL1/n02690373_3378"
+target_id1 = "aeroplaneFGL1_BGL1/n02690373_3378"
 for i in range(len(dataset.image_info)):
-    if dataset.image_info[i]['id'] == target_id_1:
+    if dataset.image_info[i]['id'] == target_id1:
         target_idx1 = i
         break
-target_id_2 = "bottleFGL1_BGL1/n02823428_2278"
+target_id2 = "bottleFGL1_BGL1/n02823428_2278"
 for i in range(len(dataset.image_info)):
-    if dataset.image_info[i]['id'] == target_id_2:
+    if dataset.image_info[i]['id'] == target_id2:
         target_idx2 = i
         break
 
-image_ids = [target_idx1, target_idx2]
+image_index = [target_idx1, target_idx2]
+image_ids = [target_id1, target_id2]
+images = []
 
-for i in range(len(image_ids)):
-    image_id = image_ids[i]
+for i in range(len(image_index)):
     image, image_meta, gt_class_id, gt_bbox, gt_mask = \
-        modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
+        modellib.load_image_gt(dataset, config, image_index[i],
+                               use_mini_mask=False)
+    info = dataset.image_info[image_index[i]]
+    images.append(image)
 
+    bboxes = extract_bboxes(gt_mask)
+    gt_mask = minimize_mask(bboxes, gt_mask, (56, 56))
     gt_mask = np.transpose(gt_mask, (2, 0, 1))
-    sample_info.append({'image_id': float(image_id),
-                        'image': image.astype('float').tolist(),
-                        'mask_gt': gt_mask.astype('float').tolist()})
+    for seg, bbox in zip(gt_mask, bboxes):
+        anno_info = {'id': image_ids[i],
+                     'file_names': image_ids[i] + '.JPEG',
+                     'width': float(info['width']),
+                     'height': float(info['height']),
+                     'mask_true': seg.astype('float').tolist(),
+                     'bbox_true': bbox.astype('float').tolist()}
+        sample_info.append(anno_info)
 
 
 # ==================== Get predicted mask ========================
@@ -80,16 +90,23 @@ model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR,
 weights_path = MODEL_PATH
 model.load_weights(weights_path, by_name=True)
 
-for i in range(len(sample_info)):
-    image = np.array(sample_info[i]['image'])
-    results = model.detect([image], verbose=1)
+images = np.array(images)
 
+num_seg = 0
+for i in range(len(images)):
+    results = model.detect([images[i]], verbose=1)
     r = results[0]
     mask_pred = r['masks']
+    bboxes = extract_bboxes(mask_pred)
+    mask_pred = minimize_mask(bboxes, mask_pred, (56, 56))
+
     mask_pred = np.transpose(mask_pred, (2, 0, 1))
-    sample_info[i].update({'mask_pred': mask_pred.astype('float').tolist()})
+    for seg, bbox in zip(gt_mask, bboxes):
+        anno_info = {'mask_pred': seg.astype('float').tolist(),
+                     'bbox_pred': bbox.astype('float').tolist()}
+        sample_info[num_seg].update(anno_info)
+        num_seg += 1
 
 
-# sample_info = {'sample_info': sample_info}
 with open('sample_mask_info.json', "w") as outfile:
     json.dump(sample_info, outfile)
