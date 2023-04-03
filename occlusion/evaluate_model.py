fb6b90034375ca904b1bd980b4dff34c6cfebffc
sunfanyi <sunfanyi_jim@outlook.com>
2023-04-03 02:45:56 +0100

    Comparison between original and modified model showing improvement.

--- occlusion/evaluate_model.py
+++ occlusion/evaluate_model.py
@@ -1,47 +1,62 @@
 #%%
-import os
-import sys
 import time
 import random
-import numpy as np
 import matplotlib
-import matplotlib.pyplot as plt
 
-from prepare_evaluation import get_ax, prepare
+from prepare_evaluation import *
 
 ROOT_DIR = os.path.abspath("../")
 sys.path.insert(0, ROOT_DIR)
-from mrcnn import utils
 from mrcnn import visualize
-from mrcnn.visualize import display_images
 import mrcnn.model as modellib
-from mrcnn.model import log
 
 MODEL_PATH = r"D:\Users\ROG\Desktop\FYP\Mask_RCNN-Occulusion\logs\train_012_bdry_120_m351\mask_rcnn_occlusion_0106.h5"
-model, config, dataset = prepare(MODEL_PATH)
+bdry = True
+model_with_bdry, _, _ = prepare(MODEL_PATH, bdry)
+MODEL_PATH = r"D:\Users\ROG\Desktop\FYP\Mask_RCNN-Occulusion\logs\train_004_120_m351\mask_rcnn_occlusion_0120.h5"
+bdry = False
+model_wo_bdry, config, dataset = prepare(MODEL_PATH, bdry)
 matplotlib.use('TkAgg')
 #%%
 start = time.time()
 plt.close('all')
 
-# image_id = random.choice(dataset.image_ids)
-for image_id in [10, 11, 12, 13]:
+target_ids = [
+    "aeroplaneFGL1_BGL1/n02690373_3378",
+    "busFGL1_BGL1/n02924116_600",
+    "bottleFGL1_BGL1/n02823428_56",
+    "busFGL1_BGL1/n02924116_11237",
+    "trainFGL1_BGL1/n02917067_1701"
+    ]
+
+image_ids = search_image_ids(dataset, target_ids)
+
+for image_id in image_ids:
+    _, axes = plt.subplots(1, 3, figsize=(16, 6))
     image, image_meta, gt_class_id, gt_bbox, gt_mask =\
         modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
     info = dataset.image_info[image_id]
     print("image ID: {}.{} ({}) {}".format(info["source"], info["id"], image_id,
                                            dataset.image_reference(image_id)))
-    results = model.detect([image], verbose=1)
 
-    # Display results
-    ax = get_ax(1)
-    r = results[0]
+    results1 = model_with_bdry.detect([image], verbose=0)
+    results2 = model_wo_bdry.detect([image], verbose=0)
+
+    r = results1[0]
     visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
-                                dataset.class_names, r['scores'], ax=ax,
-                                title="Predictions")
-    log("gt_class_id", gt_class_id)
-    log("gt_bbox", gt_bbox)
-    log("gt_mask", gt_mask)
+                                dataset.class_names, r['scores'], ax=axes[0],
+                                title="modified")
+
+    r = results2[0]
+    visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                                dataset.class_names, r['scores'], ax=axes[1],
+                                title="original")
+
+    visualize.display_instances(image, gt_bbox, gt_mask, gt_class_id,
+                                dataset.class_names, ax=axes[2],
+                                title="GT")
+
+    plt.subplots_adjust(left=0, right=1, bottom=0, wspace=0)
     plt.show()
 
 end = time.time()
--- occlusion/prepare_evaluation.py
+++ occlusion/prepare_evaluation.py
@@ -7,7 +7,6 @@ import occlusion
 
 ROOT_DIR = os.path.abspath("../")
 sys.path.insert(0, ROOT_DIR)
-import mrcnn.model as modellib
 
 config = occlusion.OcclusionConfig()
 dataset_DIR = '../../datasets/dataset_occluded'
@@ -18,12 +17,27 @@ class InferenceConfig(config.__class__):
     IMAGES_PER_GPU = 1
 
 
+def search_image_ids(dataset, target_ids):
+    image_ids = []
+    for target_id in target_ids:
+        for i in range(len(dataset.image_info)):
+            if dataset.image_info[i]['id'] == target_id:
+                image_ids.append(i)
+                break
+    return image_ids
+
+
 def get_ax(rows=1, cols=1, size=16):
     _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
     return ax
 
 
-def prepare(MODEL_PATH):
+def prepare(MODEL_PATH, bdry):
+    if bdry:
+        import mrcnn.model as modellib
+    else:
+        import mrcnn.model_wo_bdry as modellib
+
     MODEL_DIR = os.path.join(ROOT_DIR, "logs")
     config = InferenceConfig()
     DEVICE = "/gpu:0"  # /cpu:0 or /gpu:0
@@ -48,5 +62,5 @@ def prepare(MODEL_PATH):
 
 if __name__ == '__main__':
     MODEL_PATH = r"D:\Users\ROG\Desktop\FYP\Mask_RCNN-Occulusion\logs\train_012_bdry_120_m351\mask_rcnn_occlusion_0106.h5"
-    model, config, dataset = prepare(MODEL_PATH)
+    model, config, dataset = prepare(MODEL_PATH, True)
 
